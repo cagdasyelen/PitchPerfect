2233528b8ca7b7ff3d65c45b0888c3b2a5ca5b6f
Cagdas Yelen <cagdasyelen@gmail.com>
2019-12-31 13:09:53 -0800

    Added the nav controller and the logic to pass the audio file to the second view

---
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by Cagdas Yelen on 12/31/19.
+//  Copyright © 2019 Yelen. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+    
+    var recordedAudioUrl: URL!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
