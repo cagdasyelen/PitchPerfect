fbfe4a75d0a8190693b4c504fae83e14c805208d
Cagdas Yelen <cagdasyelen@gmail.com>
2019-12-31 12:08:32 -0800

    Init with the first view controller

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        stopRecordingButton.isEnabled = false
+    }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+    }
+    @IBAction func recordAudio(_ sender: Any) {
+        recordingLabel.text = "Recording in progress"
+        recordButton.isEnabled = false
+        stopRecordingButton.isEnabled = true
+        
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+        recordingLabel.text = "Tap to Record"
+    }
+    
 }
 
